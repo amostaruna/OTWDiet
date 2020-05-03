559c8574171e7c59daa4e8d279391cf6744e8747
amostaruna <amostaruna@gmail.com>
2020-05-03 22:11:39 +0700

    OTW DIET 0.1
    penambahan asset terkini
    perbaikan animasi David
    perbaikan animasi Ice Man

---
+++ OTW DIET 0.1/Assets/scripts/PlayerController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float movespeed;
+    private float moveSpeedStore;
+
+    public float speedMultiplier;
+
+    public float speedIncreaseMilestone;
+    private float speedIncreaseMilestoneStore;
+
+    private float speedMilestoneCount;
+    private float speedMilestoneStore;
+    private float speedMilestoneCountStore;
+
+    public float jumpforce;
+
+    public float jumpTime;
+    private float jumpTimeCounter;
+
+    private bool stoppedJumping;
+    private bool canDoubleJump;
+
+    private Rigidbody2D myRigidbody;
+
+    public bool grounded;
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+
+    //private Collider2D myCollider;
+    private Animator myAnimator;
+
+    public GameManager TheGameManager;
+
+    public AudioSource jumpSound;
+    public AudioSource deathSound;
+    // Use this for initialization
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        //myCollider = GetComponent<Collider2D>();
+        myAnimator = GetComponent<Animator>();
+        jumpTimeCounter = jumpTime;
+
+        speedMilestoneCount = speedIncreaseMilestone;
+
+        moveSpeedStore = movespeed;
+        speedMilestoneCountStore = speedMilestoneCount;
+        speedIncreaseMilestoneStore = speedIncreaseMilestone;
+
+        stoppedJumping = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        if (transform.position.x > speedMilestoneCount)
+        {
+            speedMilestoneCount += speedIncreaseMilestone;
+            speedIncreaseMilestone = speedIncreaseMilestone * speedMultiplier;         
+            movespeed = movespeed * speedMultiplier;
+        }
+        myRigidbody.velocity = new Vector2(movespeed, myRigidbody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            if (grounded)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpforce);
+                stoppedJumping = false;
+                jumpSound.Play();
+            }
+            if(!grounded && canDoubleJump)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpforce);
+                jumpTimeCounter = jumpTime;
+                stoppedJumping = false;
+                canDoubleJump = false;
+                jumpSound.Play();
+            }
+        }
+        if(Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0) && !stoppedJumping)
+        {
+            if(jumpTimeCounter>0)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpforce);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+        }
+        if(Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0))
+        {
+            jumpTimeCounter = 0;
+            stoppedJumping = true;
+        }
+
+        if(grounded)
+        {
+            jumpTimeCounter = jumpTime;
+            canDoubleJump = true;
+        }
+        myAnimator.SetFloat("Speed", myRigidbody.velocity.x);
+        myAnimator.SetBool("Grounded", grounded);
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Killbox")
+        {
+            TheGameManager.restartGame();
+            movespeed = moveSpeedStore;
+            speedMilestoneCount = speedMilestoneCountStore;
+            speedIncreaseMilestone = speedIncreaseMilestoneStore;
+            deathSound.Play();
+        }
+    }
+}
