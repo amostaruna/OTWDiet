46607228e086689c9ff882a78b048268f0cede02
amostaruna <amostaruna@gmail.com>
2020-05-19 15:44:26 +0700

    OTW Diet 0.3
    Penambahan fitur nyawa pada David berupa 3 buah hati. Jika david terkena musuh atau jatuh maka jumlah hati akan berkurang 1. Jika hati habis maka permainan selesai

---
+++ OTW Diet 0.3/Assets/scripts/PlayerController.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float movespeed;
+    private float moveSpeedStore;
+
+    public float speedMultiplier;
+
+    public float speedIncreaseMilestone;
+    private float speedIncreaseMilestoneStore;
+
+    private float speedMilestoneCount;
+    private float speedMilestoneStore;
+    private float speedMilestoneCountStore;
+
+    public float jumpforce;
+
+    public float jumpTime;
+    private float jumpTimeCounter;
+
+    private bool stoppedJumping;
+    private bool canDoubleJump;
+
+    private Rigidbody2D myRigidbody;
+
+    public bool grounded;
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+
+    //private Collider2D myCollider;
+    private Animator myAnimator;
+
+    public GameManager TheGameManager;
+
+    //sound effect
+    public AudioSource jumpSound;
+    public AudioSource deathSound;
+
+    //heart
+    public GameObject heart1, heart2, heart3;
+    public static int health;
+    // Use this for initialization
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        //myCollider = GetComponent<Collider2D>();
+        myAnimator = GetComponent<Animator>();
+        jumpTimeCounter = jumpTime;
+
+        speedMilestoneCount = speedIncreaseMilestone;
+
+        moveSpeedStore = movespeed;
+        speedMilestoneCountStore = speedMilestoneCount;
+        speedIncreaseMilestoneStore = speedIncreaseMilestone;
+
+        health = 3;
+        heart1.gameObject.SetActive(true);
+        heart2.gameObject.SetActive(true);
+        heart3.gameObject.SetActive(true);
+
+        stoppedJumping = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        if (transform.position.x > speedMilestoneCount)
+        {
+            speedMilestoneCount += speedIncreaseMilestone;
+            speedIncreaseMilestone = speedIncreaseMilestone * speedMultiplier;         
+            movespeed = movespeed * speedMultiplier;
+        }
+        myRigidbody.velocity = new Vector2(movespeed, myRigidbody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            if (grounded)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpforce);
+                stoppedJumping = false;
+                jumpSound.Play();
+            }
+            if(!grounded && canDoubleJump)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpforce);
+                jumpTimeCounter = jumpTime;
+                stoppedJumping = false;
+                canDoubleJump = false;
+                jumpSound.Play();
+            }
+        }
+        if(Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0) && !stoppedJumping)
+        {
+            if(jumpTimeCounter>0)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpforce);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+        }
+        if(Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0))
+        {
+            jumpTimeCounter = 0;
+            stoppedJumping = true;
+        }
+
+        if(grounded)
+        {
+            jumpTimeCounter = jumpTime;
+            canDoubleJump = true;
+        }
+        myAnimator.SetFloat("Speed", myRigidbody.velocity.x);
+        myAnimator.SetBool("Grounded", grounded);
+        if (health > 3)
+            health = 3;
+        switch (health)
+        {
+            case 3:
+                heart1.gameObject.SetActive(true);
+                heart2.gameObject.SetActive(true);
+                heart3.gameObject.SetActive(true);
+                break;
+            case 2:
+                heart1.gameObject.SetActive(true);
+                heart2.gameObject.SetActive(true);
+                heart3.gameObject.SetActive(false);
+                break;
+            case 1:
+                heart1.gameObject.SetActive(true);
+                heart2.gameObject.SetActive(false);
+                heart3.gameObject.SetActive(false);
+                break;
+            case 0:
+                heart1.gameObject.SetActive(false);
+                heart2.gameObject.SetActive(false);
+                heart3.gameObject.SetActive(false);
+                TheGameManager.restartGame();
+                movespeed = moveSpeedStore;
+                speedMilestoneCount = speedMilestoneCountStore;
+                speedIncreaseMilestone = speedIncreaseMilestoneStore;
+
+                health = 3;
+                break;
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Killbox")
+        {
+            health--;
+            deathSound.Play();
+        }
+    }
+}
---
+++ OTW Diet 0.3/Assets/scripts/Powerups.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerups : MonoBehaviour
+{
+    public bool doublePoints;
+    public bool saveMode;
+
+    public float powerUpLength;
+    private PowerupManager thePowerupManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thePowerupManager = FindObjectOfType<PowerupManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "player")
+        {
+            thePowerupManager.ActivatePowerUp(doublePoints, saveMode, powerUpLength);
+
+        }
+        gameObject.SetActive(false);
+    }
+
+}
